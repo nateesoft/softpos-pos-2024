d04fe82553daa33c46082541b3654a12f4b9e1b6
Nathee Phuwong <natee.live@gmail.com>
2025-01-23 08:08:48 +0700

    test printer driver

--- pos-restaurant-service/routes/pos/pos_setting.js
+++ pos-restaurant-service/routes/pos/pos_setting.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getPosSetting, createPosSetting, updatePosSetting } = require('../../services/management/PosSettingService');
+const { getPosSetting, createPosSetting, updatePosSetting, getPosSettingByTerminal } = require('../../services/management/PosSettingService');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
@@ -24,6 +24,17 @@ router.post('/', function (req, res, next) {
 
 router.put('/', function (req, res, next) {
   updatePosSetting(req.body)
+    .then(rows => {
+      res.status(200).json({ status: 2000, data: rows })
+    })
+    .catch(err => {
+      res.status(500).json({ status: 5000, data: null, errorMessage: err.message })
+    })
+});
+
+router.get('/:terminalId', function (req, res, next) {
+  const { terminalId } = req.params
+  getPosSettingByTerminal(terminalId)
     .then(rows => {
       res.status(200).json({ status: 2000, data: rows })
     })
--- pos-restaurant-service/services/management/PosSettingService.js
+++ pos-restaurant-service/services/management/PosSettingService.js
@@ -14,26 +14,19 @@ const createPosSetting = async (payload) => {
     currency_use_rate,
     currency_use,
     receipt_printer_ip,
-    kichen_printer_ip
+    kichen_printer_ip,
+    create_date,
+    user_update,
+    terminal_id
   } = payload
-  const sql = `INSERT INTO pos_setting 
-      (id,language_main,language_main_title,language_use,language_use_title,
+  const sql = `INSERT INTO pos_setting (id,language_main,language_main_title,language_use,language_use_title,
         timezone_main,timezone_use,currency_baht_rate,currency_baht,
-        currency_use_rate,currency_use,receipt_printer_ip,kichen_printer_ip) 
-      VALUES ('${id}',
-        '${language_main}',
-        '${language_main_title}',
-        '${language_use}',
-        '${language_use_title}',
-        '${timezone_main}',
-        '${timezone_use}',
-        '${currency_baht_rate}',
-        '${currency_baht}',
-        '${currency_use_rate}',
-        '${currency_use}',
-        '${receipt_printer_ip}',
-        '${kichen_printer_ip}')`
-        console.log(sql)
+        currency_use_rate,currency_use,receipt_printer_ip,kichen_printer_ip,
+        create_date, user_update, terminal_id) 
+      VALUES ('${id}', '${language_main}', '${language_main_title}', '${language_use}', '${language_use_title}',
+        '${timezone_main}', '${timezone_use}', '${currency_baht_rate}', '${currency_baht}', '${currency_use_rate}',
+        '${currency_use}', '${receipt_printer_ip}', '${kichen_printer_ip}',
+        '${create_date}', '${user_update}', '${terminal_id}')`
   const results = await pool.query(sql)
   return results
 }
@@ -52,28 +45,40 @@ const updatePosSetting = async (payload) => {
     currency_use_rate,
     currency_use,
     receipt_printer_ip,
-    kichen_printer_ip
+    kichen_printer_ip,
+    update_date,
+    user_update,
+    terminal_id
   } = payload
   const sql = `UPDATE pos_setting 
-            SET language_main='${language_main}',
-              language_main_title='${language_main_title}',
-              language_use='${language_use}',
-              language_use_title='${language_use_title}',
-              timezone_main='${timezone_main}',
-              timezone_use='${timezone_use}',
-              currency_baht_rate='${currency_baht_rate}',
-              currency_baht='${currency_baht}',
-              currency_use_rate='${currency_use_rate}',
-              currency_use='${currency_use}',
-              receipt_printer_ip='${receipt_printer_ip}',
-              kichen_printer_ip='${kichen_printer_ip}' 
-              WHERE id='${id}'`
+      SET language_main='${language_main}',
+        language_main_title='${language_main_title}',
+        language_use='${language_use}',
+        language_use_title='${language_use_title}',
+        timezone_main='${timezone_main}',
+        timezone_use='${timezone_use}',
+        currency_baht_rate='${currency_baht_rate}',
+        currency_baht='${currency_baht}',
+        currency_use_rate='${currency_use_rate}',
+        currency_use='${currency_use}',
+        receipt_printer_ip='${receipt_printer_ip}',
+        kichen_printer_ip='${kichen_printer_ip}',
+        update_date='${update_date}',
+        user_update='${user_update}',
+        terminal_id='${terminal_id}' 
+        WHERE id='${id}'`
   const results = await pool.query(sql)
   return results
 }
 
 const getPosSetting = async () => {
   const sql = `select * from pos_setting`
+  const results = await pool.query(sql)
+  return results
+}
+
+const getPosSettingByTerminal = async (terminal_id) => {
+  const sql = `select * from pos_setting where terminal_id='${terminal_id}'`
   const results = await pool.query(sql)
   if (results.length > 0) {
     return results[0]
@@ -84,5 +89,6 @@ const getPosSetting = async () => {
 module.exports = {
   createPosSetting,
   updatePosSetting,
-  getPosSetting
+  getPosSetting,
+  getPosSettingByTerminal
 }
