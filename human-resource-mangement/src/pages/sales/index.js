02479dce817aa70df65ab5d351bc98a06daa2f8a
นที ภูวงษ <natee.live@gmail.com>
2025-03-06 19:59:52 +0700

    rename all folder

---
+++ human-resource-mangement/src/pages/sales/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SalePage = () => {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default SalePage
---
+++ pos-restaurant/src/index.js
@@ -0,0 +1,38 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { I18nextProvider } from "react-i18next"
+import i18next from "i18next"
+import { HelmetProvider } from "react-helmet-async"
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import global_th from "./locales/th/global.json"
+import global_en from "./locales/en/global.json"
+
+import App from "./App"
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "auto",
+  fallbackLng: "en",
+  resources: {
+    en: {
+      global: global_en
+    },
+    th: {
+      global: global_th
+    }
+  }
+})
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(
+  <HelmetProvider>
+    <I18nextProvider i18n={i18next}>
+      <ThemeProvider theme={{}}>
+        <CssBaseline enableColorSchem />
+          <App />
+      </ThemeProvider>
+    </I18nextProvider>
+  </HelmetProvider>
+)
